4802cfc1d5289e3d5734fff7ea7b68e45210fb41
AbhishekBansal <bansalabhishek7411@gmail.com>
2023-09-01 22:23:45 +0530

    bugs

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -17,7 +17,7 @@ const signUp = async (req, res) => {
   try {
     const existingUser = await usermodel.findOne({ phone: number });
     if (existingUser) {
-      return res.status(400).json({ message: `User with ${number} already exists` });
+      return res.status(200).json({ message: `User with ${number} already exists` });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
